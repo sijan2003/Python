6c87982ffb4d975d167fc8db8e603023c0c22630
sijan2003 <122282540+sijan2003@users.noreply.github.com>
2023-07-08 15:19:47 +0545

    Add files via upload

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,8 @@
 import random
 
-A = ['▩' for _ in range(26)]
-B = [i for i in range(0, 26)]
-C = ['▩' for _ in range(26)]
+A = ['▩' for _ in range(25)]
+B = [i for i in range(0, 25)]
+C = ['▩' for _ in range(25)]
 
 def clear_screen():
     print("\033[H\033[J")
@@ -22,7 +22,8 @@ def board():
     print('-' * 40, '    ', '-' * 40)
 
 def set_bombs(initial_guess):
-    bomb_indices = random.sample(range(len(A)), 6)
+    # Use range(25) instead of range(len(A))
+    bomb_indices = random.sample(range(25), 6)
     if initial_guess in bomb_indices:
         bomb_indices.remove(initial_guess)
     for index in bomb_indices:
@@ -43,33 +44,56 @@ def display_board():
         print()
     print('-' * 40, '    ', '-' * 40)
 
-def mechanics():
+def count_bombs(index):
+    # Create a separate function that returns a count of bombs among the neighboring cells
     count = 0
+    row = index // 5
+    col = index % 5
+
+    if col > 0 and A[index - 1] == '&':
+        count += 1
+    if col < 4 and A[index + 1] == '&':
+        count += 1
+    if row > 0 and A[index - 5] == '&':
+        count += 1
+    if row < 4 and A[index + 5] == '&':
+        count += 1
+    if col > 0 and row > 0 and A[index - 6] == '&':
+        count += 1
+    if col < 4 and row > 0 and A[index - 4] == '&':
+        count += 1
+    if col > 0 and row < 4 and A[index + 4] == '&':
+        count += 1
+    if col < 4 and row < 4 and A[index + 6] == '&':
+        count += 1
+
+    return count
+
+def mechanics():
     display_board()
-    a = int(input("Choose a number: "))
+    
+    # Use a try-except block to handle invalid inputs
+    while True:
+        try:
+            a = int(input("Choose a number: "))
+            if not (0 <= a < 25):
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number between 0 and 24.")
+
     if A[a] == '&':
         print("Game Over! You hit a bomb!")
         return False
+
     # Check for win condition
     if C.count('▩') == 6:
         print("Congratulations! You won the game!")
         return False
-    if a % 5 != 0 and A[a - 1] == '&':
-        count += 1
-    if a % 5 != 4 and A[a + 1] == '&':
-        count += 1
-    if a >= 5 and A[a - 5] == '&':
-        count += 1
-    if a < 20 and A[a + 5] == '&':
-        count += 1
-    if a % 5 != 0 and a >= 5 and A[a - 6] == '&':
-        count += 1
-    if a % 5 != 4 and a >= 5 and A[a - 4] == '&':
-        count += 1
-    if a % 5 != 0 and a < 20 and A[a + 4] == '&':
-        count += 1
-    if a % 5 != 4 and a < 20 and A[a + 6] == '&':
-        count += 1
+
+    # Use the count_bombs function to get the count of bombs
+    count = count_bombs(a)
+
     C[a] = str(count)
     B[a] = 'X'
     return True
@@ -77,34 +101,17 @@ def mechanics():
 def first_move(ui):
     mechanics()
 
-    for _ in range(8):
-        while True:
-            randomj = random.randint(0, 24)
-            if C[randomj] == '▩' and randomj != ui:
-                break
+    # Create a list of all the valid indices that are not the initial guess or a bomb
+    valid_indices = [i for i in range(25) if i != ui and A[i] != '&']
 
-        count = 0
-        row = randomj // 5
-        col = randomj % 5
+    # Use random.sample to select eight of them to reveal
+    reveal_indices = random.sample(valid_indices, 8)
 
-        if col > 0 and A[randomj - 1] == '&':
-            count += 1
-        if col < 4 and A[randomj + 1] == '&':
-            count += 1
-        if row > 0 and A[randomj - 5] == '&':
-            count += 1
-        if row < 4 and A[randomj + 5] == '&':
-            count += 1
-        if col > 0 and row > 0 and A[randomj - 6] == '&':
-            count += 1
-        if col < 4 and row > 0 and A[randomj - 4] == '&':
-            count += 1
-        if col > 0 and row < 4 and A[randomj + 4] == '&':
-            count += 1
-        if col < 4 and row < 4 and A[randomj + 6] == '&':
-            count += 1
+    for index in reveal_indices:
+        # Use the count_bombs function to get the count of bombs
+        count = count_bombs(index)
 
-        C[randomj] = str(count)
+        C[index] = str(count)
 
     display_board()
 
